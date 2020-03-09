d0c40242b71f41f833f87787bdaeb0a642d29346
IvanKuzyshyn <kuzyshyn.ivan@gmail.com>
2020-03-09 16:31:28 +0200

    Add one_to_many association between user and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_many :comments
+  has_many :articles
+
+  before_save { self.email = email.downcase }
+
   validates :first_name, presence: true, length: { minimum: 2 }
   validates :last_name, presence: true, length: { minimum: 2 }
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "should be valid" }
